09942f0339615b482a1251b7353eac0f2246f755
Hamed-68 <hamedbakhti918@yahoo.com>
2022-09-02 01:05:27 +0430

    add authtoken

--- blogham/urls.py
+++ blogham/urls.py
@@ -18,10 +18,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.authtoken import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-token-auth/', views.obtain_auth_token),
     path('posts/', include('post.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),

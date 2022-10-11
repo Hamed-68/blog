cf28dd14d3d9dc8aedbf78aabff6c01804f616ce
Hamed-68 <hamedbakhti918@yahoo.com>
2022-10-11 16:23:54 +0330

    remove comment update

--- post/models.py
+++ post/models.py
@@ -61,7 +61,6 @@ class Comment(models.Model):
                             related_name='comments')
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.body[:15]

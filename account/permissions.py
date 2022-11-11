6c6f2e2bce4caa8cbd1867070c5143d197abb2c6
Hamed-68 <hamedbakhti918@yahoo.com>
2022-11-11 21:20:34 +0330

    edit permission for users account

--- account/permissions.py
+++ account/permissions.py
@@ -1,11 +1,17 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class CreateOrNeedAuthenticate(BasePermission):
 
+    def has_permission(self, request, view):
+        if request.method == 'POST': # just anonymous users can create account
+            return not request.user.is_authenticated
+        elif request.method in SAFE_METHODS: # safemethods are allowed for anonymous users
+            return True
+        return request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
-        if request.method == 'DELETE':
-            return request.user.is_staff or request.user == obj
-        elif request.method == 'PUT' or request.method == 'PATCH':
+        private_methods = ['PUT', 'PATCH', 'DELETE']
+        if request.method in private_methods: # just owner accounts can delete or edit account
             return request.user == obj
         return True
